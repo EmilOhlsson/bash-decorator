cbca008a6fa5de4477315c6752f96ec712ea94ec
Emil Ohlsson <kotte@kottland.net>
2017-10-10 22:35:22 +0200

    Initial stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate git2;
+
+use git2::Repository;
+
+fn git_decoration() -> Result<String, git2::Error> {
+    let repo = Repository::open(".")?;
+    let head = repo.head()?;
+
+    Ok(String::from(head.name().unwrap_or("unknown")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", git_decoration().unwrap());
 }
